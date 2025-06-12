262fe011cdcf1de881f20bc6b43dcd1de1629301
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2025-06-12 22:23:49 +0200

    simplify

--- scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-# Empty __init__.py file to make the scripts directory a Python package
--- validate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Simple script to validate a CAD manifest against a schema
-# This is now a wrapper for the new script in the scripts directory
-
-# Forward arguments to the new script
-./scripts/validate-uvx.sh "$@"
